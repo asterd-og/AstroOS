ed5f8378a7541c7f2d5b54f5e4b8a99499325f12
astrido <asterdog@outlook.com>
2023-02-20 00:44:26 -0300

    Keyboard shift and caps

--- src/sys/ps2/keyboard.cpp
+++ src/sys/ps2/keyboard.cpp
@@ -6,12 +6,29 @@
 namespace Keyboard {
     bool pressed = false;
     char currentChar = '\0';
+    bool caps = false;
+    bool shift = false;
 
     void handler(Registers* regs) {
         uint8_t key = inb(0x60);
         if (!(key & 0x80)) {
-            pressed = true;
-            currentChar = keys[key];
+            if (key == 0x2a) {
+                // shift
+                shift = true;
+            } else if (key == 0x3a) {
+                // caps
+                caps = !caps;
+            } else {
+                pressed = true;
+                if (shift) currentChar = keysShift[key];
+                else if (caps) currentChar = keysCaps[key];
+                else currentChar = keys[key];
+            }
+        } else {
+            if (key == 0xaa) {
+                // shift up
+                shift = false;
+            }
         }
     }
 
